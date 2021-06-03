881b5b867b230a7c7ea32cb93b5cc70a82703553
Alois Mbutura <aloismbutura@gmail.com>
2021-06-03 15:56:55 +0300

    Update I2C bus driver
    Update I2C driver bus scanning functionality. This update allows
    the controller to additionally visualize timeouts on the bus during
    scanning. Previously only success or failure was captured without
    context.

--- firmware/components/hwInterface/i2c.cpp
+++ firmware/components/hwInterface/i2c.cpp
@@ -31,7 +31,7 @@ m_sdaPin(sdaPin), m_sclPin(sclPin), m_portNum(portNum)
 	conf.sda_pullup_en    = (pullupsState == pullupsEnable) ? GPIO_PULLUP_ENABLE : GPIO_PULLUP_DISABLE;
 	conf.scl_pullup_en    = (pullupsState == pullupsDisable) ? GPIO_PULLUP_ENABLE : GPIO_PULLUP_DISABLE;
 	conf.master.clk_speed = clockSpeed;
-	conf.clk_flags        = 0; /*Choose a clock only according to desired frequencies*/
+	//conf.clk_flags        = 0; /*Choose a clock only according to desired frequencies*/
 	esp_err_t err = i2c_param_config(m_portNum, &conf);
 	if (err != ESP_OK)
     {
@@ -50,20 +50,35 @@ m_sdaPin(sdaPin), m_sclPin(sclPin), m_portNum(portNum)
 
 void externalHardwareInterface::i2cBus::scanBusAddresses()
 {
-	printf("Data Pin: %d, Clock Pin: %d\n", this->m_sdaPin, this->m_sclPin);
-	printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n");
-	printf("00:         ");
-	for (uint8_t i = 3; i < 0x78; i++) {
-		if (i % 16 == 0) {
-			printf("\n%.2x:", i);
-		}
-		if (isPresent(i)) {
-			printf(" %.2x", i);
-		} else {
-			printf(" --");
-		}
-	}
-	printf("\n");
+	printf("Data Pin: %d, Clock Pin: %d\n", m_sdaPin, m_sclPin);
+    uint8_t address;
+    printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\r\n");
+    for (int i = 0; i < 128; i += 16) {
+        printf("%02x: ", i);
+        for (int j = 0; j < 16; j++) {
+            fflush(stdout);
+            address = i + j;
+            i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+            i2c_master_start(cmd);
+            i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_WRITE, 1 /* expect ack */);
+            i2c_master_stop(cmd);
+            esp_err_t ret = i2c_master_cmd_begin(m_portNum, cmd, 50 / portTICK_RATE_MS);
+            i2c_cmd_link_delete(cmd);
+            if (ret == ESP_OK) 
+			{
+                printf("%02x ", address);
+            } 
+			else if (ret == ESP_ERR_TIMEOUT)
+			{
+                printf("UU ");
+            }
+			else
+			{
+                printf("-- ");
+            }
+        }
+        printf("\r\n");
+    }
 }
 
 bool externalHardwareInterface::i2cBus::isPresent(uint8_t address)
@@ -73,7 +88,7 @@ bool externalHardwareInterface::i2cBus::isPresent(uint8_t address)
 	i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_WRITE, 1 /* expect ack */);
 	i2c_master_stop(cmd);
 
-	esp_err_t espRc = ::i2c_master_cmd_begin(m_portNum, cmd, 100 / portTICK_PERIOD_MS);
+	esp_err_t ret = ::i2c_master_cmd_begin(m_portNum, cmd, 50 / portTICK_PERIOD_MS);
 	i2c_cmd_link_delete(cmd);
-	return espRc == 0;  // Return true if the slave is present and false otherwise.
+	return ret == 0;  // Return true if the slave is present and false otherwise.
 } 
