a1d98b6187c124f3513ab12237c4bd6d60769ce9
Alois Mbutura <aloismbutura@gmail.com>
2021-08-04 01:16:59 +0300

    Make applyCorrectionFactors() private

--- firmware/components/hwSubsystem_particulateSensor/include/correction_factors.hpp
+++ firmware/components/hwSubsystem_particulateSensor/include/correction_factors.hpp
@@ -3,8 +3,8 @@ namespace externalHardwareSubsystem
 {
   namespace particulateSensor
   {
-  constexpr float slope = 1.0;
-  constexpr float intercept = 0.0;
+  constexpr float slope = 1.1370567863062169;
+  constexpr float intercept = 3.6062330593909735;
   }
 }
   
