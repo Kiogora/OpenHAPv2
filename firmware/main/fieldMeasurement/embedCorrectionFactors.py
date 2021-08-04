0903450e6e824d9b5299272745f3429b94fe2252
Alois Mbutura <aloismbutura@gmail.com>
2021-08-04 10:14:57 +0300

    Update embedCorrectionFactors.py
    Add function to print usage information.
    Add ability to pass device port as a command line argument.

--- firmware/main/fieldMeasurement/embedCorrectionFactors.py
+++ firmware/main/fieldMeasurement/embedCorrectionFactors.py
@@ -1,77 +1,88 @@
-import subprocess
-import csv
-import os
+import subprocess, csv, os
+
+def show_help():
+    print 'embedCorrectionFactors.py - Generates headers with PM sensor adjustment factors, for the connected device'
+    print '  Usage: python embedCorrectionFactors.py [device port]'
+    print '  Examples:'
+    print '    python embedCorrectionFactors.py /dev/ttyUSB0'
+    print '    python embedCorrectionFactors.py /dev/ttyUSB1'
 
 # Read Base MAC address of connected device
-def get_mac():
-  process = subprocess.Popen('esptool -p /dev/ttyUSB0 read_mac'.split(), 
-                              stdout=subprocess.PIPE, 
-                              stderr=subprocess.PIPE)
-  substr = 'MAC: '
-  stdout, stderr = process.communicate()
-  split_stdout = stdout.decode().split('\n')
-  filtered = [line for line in split_stdout if substr in line]
-  if len(filtered) == 0:
-    return -1
-  else: 
-    mac = filtered[0].replace(substr, '')
+def get_mac(device_port):
+    substr = 'MAC: '
+    
+    process = subprocess.Popen(f'esptool -p {device_port} read_mac'.split(), 
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    split_stdout = stdout.decode().split('\n')
+    filtered = [line for line in split_stdout if substr in line]
+    
+    if len(filtered) == 0:
+        return -1
+    else: 
+        mac = filtered[0].replace(substr, '')
     return mac
 
 #Retrieve correction factors from CSV
-def retrieve_correction_factors(mac_address):
-  file_dir = os.path.dirname(os.path.abspath(__file__))
-  path = ( 
-          f'{file_dir}/../../../'
-          'software/particulateSensorNormalization/results/correction_factors.csv'
-  )
-  try:
+def retrieve_correction_factors(mac_address, csv_path):
+    found_mac = False
+
+    try:
     with open(path, mode='r') as csv_file:
-      found_mac = False
-      csv_reader = csv.DictReader(csv_file)
-      for row in csv_reader:
-        if mac_address in row.keys():
-          found_mac = True
-          if 'intercept' in row.values():
-            intercept = row[mac_address]
-          if 'slope' in row.values():
-            slope = row[mac_address]
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            if mac_address in row.keys():
+                found_mac = True
+            if 'intercept' in row.values():
+                intercept = row[mac_address]
+            if 'slope' in row.values():
+                slope = row[mac_address]
         if found_mac == False:
-          intercept = 0.0
-          slope = 1.0
-  except:
-    print("Unable to source correction factors file")
-  finally:
-    return slope, intercept
+            intercept = 0.0
+            slope = 1.0
+    except:
+        print("Unable to source correction factors file")
+    finally:
+        return slope, intercept
 
 # Write code into file within the particulate sensor driver folder
-def generate_source(slope, intercept):
-  obrace = '{'
-  cbrace = '}'
-
-  code = fr'''
-namespace externalHardwareSubsystem
-{obrace}
-  namespace particulateSensor
-  {obrace}
-  constexpr float slope = {slope};
-  constexpr float intercept = {intercept};
-  {cbrace}
-{cbrace}
-  '''
-  file_dir = os.path.dirname(os.path.abspath(__file__))
-  path = ( 
-          f'{file_dir}/../../'
-          'components/hwSubsystem_particulateSensor/include/correction_factors.hpp'
-  )
-  with open(path,'w') as output_file:
-      output_file.write(code)
+def generate_correction_factors_hpp(slope, intercept, hpp_path):
+    code =  'namespace externalHardwareSubsystem\n'
+            '{\n'
+            '    namespace particulateSensor\n'
+            '    {\n'
+           f'        constexpr float slope = {slope};\n'
+           f'        constexpr float intercept = {intercept};\n'
+            '    }\n'
+            '}\n'
+  
+  with open(hpp_path,'w') as output_file:
+        output_file.write(code)
 
 if __name__ == "__main__":
-  result = get_mac()
-  if result != -1:
-    factors = retrieve_correction_factors(result)
-    slope = factors[0]
-    intercept = factors[1]
-    generate_source(slope, intercept)
-  else:
-    print('Error obtaining base MAC address')
+    # we need at least the port name
+    if (len(sys.argv) < 2):
+        show_help()
+        sys.exit()
+    
+    file_dir = os.path.dirname(os.path.abspath(__file__))  
+    csv_path = 
+    ( 
+    f'{file_dir}/../../../'
+    'software/particulateSensorNormalization/results/correction_factors.csv'
+    )
+    hpp_path = 
+    ( 
+    f'{file_dir}/../../'
+    'components/hwSubsystem_particulateSensor/include/correction_factors.hpp'
+    )
+  
+    device_port = sys.argv[1]
+    mac_result = get_mac(device_port)
+    if mac_result == -1:
+        print('Error obtaining device base MAC address')
+    else:
+        factors = retrieve_correction_factors(mac_result, csv_path)
+        slope = factors[0]
+        intercept = factors[1]
+        generate_correction_factors_hpp(slope, intercept, hpp_path)
