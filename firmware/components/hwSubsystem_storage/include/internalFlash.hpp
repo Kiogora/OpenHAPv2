5f4e0f2ee0d593f6acb7e706118eeae4f0201ef9
Alois Mbutura <aloismbutura@gmail.com>
2021-08-10 12:33:45 +0300

    Add CSV writing method to internal FS class

--- firmware/components/hwSubsystem_storage/include/internalFlash.hpp
+++ firmware/components/hwSubsystem_storage/include/internalFlash.hpp
@@ -8,16 +8,16 @@ namespace internalHardwareSubsystem
 {
     namespace storage
     {
+    
     class spiFlashFilesystem
     {
     public:
-        const std::string TAG = "internalHardwareSubystem::filesystems";
-
         const std::string mountPoint;
 
         spiFlashFilesystem(const std::string mountPoint = "/internal", size_t maxFiles = 5, bool formatIfMountFails = true);
         esp_err_t getBytesAvailable(size_t& usedBytes, size_t& totalBytes);
-        esp_err_t  printBytesAvailable();
+        esp_err_t printBytesAvailable();
+        esp_err_t writeCSV(uint32_t time_now, std::string macAddress, int16_t averageRssi, float ParticulateConcentration, float maxTemp);
 
     private:
     
--- firmware/components/hwSubsystem_storage/internalFlash.cpp
+++ firmware/components/hwSubsystem_storage/internalFlash.cpp
@@ -1,16 +1,18 @@
 #include <cstddef>
 #include <string>
+#include <sys/stat.h>
+#include <sys/unistd.h>
+
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_spiffs.h"
 #include "internalFlash.hpp"
-#include <fstream>
+
+static const char* TAG = "internalHardwareSubsystem::storage";
 
 internalHardwareSubsystem::storage::spiFlashFilesystem::spiFlashFilesystem(std::string mountPoint, size_t maxFiles, bool formatIfMountFails)
 : mountPoint(mountPoint)
 {
-    ESP_LOGI(TAG.c_str(), "Initializing SPIFFS");
-
     esp_vfs_spiffs_conf_t fsConfiguration =
     {
     .base_path = mountPoint.c_str(),
@@ -25,13 +27,13 @@ internalHardwareSubsystem::storage::spiFlashFilesystem::spiFlashFilesystem(std::
     {
         if (ret == ESP_FAIL) 
         {
-            ESP_LOGE(TAG.c_str(), "Failed to mount or format filesystem");
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
         } else if (ret == ESP_ERR_NOT_FOUND)
         {
-            ESP_LOGE(TAG.c_str(), "Failed to find SPIFFS partition");
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
         } else
         {
-            ESP_LOGE(TAG.c_str(), "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
         }
     }   
 
@@ -50,11 +52,71 @@ esp_err_t internalHardwareSubsystem::storage::spiFlashFilesystem::printBytesAvai
     esp_err_t ret = esp_spiffs_info(NULL, &total, &used);
     if (ret != ESP_OK) 
     {
-        ESP_LOGE(TAG.c_str(), "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
     } 
     else 
     {
-        ESP_LOGI(TAG.c_str(), "Partition size: total: %d, used: %d", total, used);
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
     }
     return ret;
+}
+
+esp_err_t internalHardwareSubsystem::storage::spiFlashFilesystem::writeCSV(uint32_t time_now, std::string macAddress, int16_t averageRssi, float particulateConcentration, float maxTemp)
+{
+    int i;
+    struct stat st;
+    FILE* file = NULL;
+    bool fileExists = false;
+    const char* fileName = (mountPoint+"/measurement.csv").c_str();
+
+    /*Open file by append mode, if file does not exist, create it*/
+    if (stat(fileName, &st) == 0)
+    {
+        /*File exists, set integer to be checked*/
+        ESP_LOGI(TAG, "File exists, appending to file");
+        fileExists = true; 
+    }
+    else
+    {
+        /*File exists, set integer to be checked*/
+        ESP_LOGW(TAG, "File does not exist, creating it...");
+        fileExists = true;           
+    }
+
+    if(fileExists)
+    {
+        file = fopen(fileName, "a");
+        if (file == NULL)
+        {
+            
+            ESP_LOGE(TAG, "Failed to open file for writing");
+            return ESP_FAIL;
+        }
+        i = fprintf(file, "\"%u\",\"%s\",\"%d\",\"%.2f\",\"%.2f\"", time_now, macAddress.c_str(), averageRssi, 
+                                                                      particulateConcentration, maxTemp);
+
+        if(i < 0)
+        {
+            ESP_LOGE(TAG, "Failed to write to opened file");
+            return ESP_FAIL;
+        }
+    }
+    else
+    {
+        file = fopen(fileName, "a");
+        if (file == NULL)
+        {
+            
+            ESP_LOGE(TAG, "Failed to open file for writing");
+            return ESP_FAIL;
+        }
+        i = fprintf(file, "\"Unix time\",\"Tag\",\"Mean RSSI\",\"PM 2.5\",\"Max viewable temperature\"\n");
+
+        if(i < 0)
+        {
+            ESP_LOGE(TAG, "Failed to write to opened file");
+            return ESP_FAIL;
+        }
+    }
+    return ESP_OK;
 }
